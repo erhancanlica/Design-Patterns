ab9204c78c91a7f56f3b6cc77d58b6269e7c4c9d
Erhan <erhancnlc@outlook.com>
2022-01-20 10:48:48 +0300

    proxy design pattern

---
+++ structural/patterns/proxy/CommandExecutorImpl.java
@@ -0,0 +1,9 @@
+package structural.patterns.proxy;
+
+public class CommandExecutorImpl implements CommandExecutor {
+    @Override
+    public void runCommand(String cmd) throws Exception {
+        Runtime.getRuntime().exec(cmd);
+        System.out.println("'" + cmd + "' command executed.");
+    }
+}
